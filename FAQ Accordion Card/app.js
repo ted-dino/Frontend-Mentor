39705009e907ace0a75353a27af1722339992ea2
Ted Dino <ted23@tutanota.com>
2022-02-02 21:52:20 +0800

    fix accesibility

--- FAQ Accordion Card/app.js
+++ FAQ Accordion Card/app.js
@@ -1,4 +1,4 @@
-const items = document.querySelectorAll("#accordion-button");
+const items = document.querySelectorAll(".accordion-button");
 
 function toggleAccordion() {
   const itemToggle = this.getAttribute("aria-expanded");
